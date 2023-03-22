a289b3d59fe2a021fd9fc456edc8cf233b3bdaef
ZewdieMc <zewdiehabtie26@gmail.com>
2023-03-22 18:33:07 +0300

    Create state and pass it to child components as prop

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import Result from './Result';
 import Keypad from './Keypad';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="calculator">
-    <Result result="0" />
-    <Keypad />
-  </div>
-);
+const Calculator = () => {
+  const [state, setState] = useState({ total: '0', next: '', operation: '' });
+
+  const { total, next, operation } = state;
+
+  const handleButtonClicks = (buttonName) => {
+    setState(calculate({ total, next, operation }, buttonName));
+    // !still confused about what object to pass
+  };
+  return (
+    <div className="calculator">
+      <Result state={state} />
+      <Keypad onButtonClick={handleButtonClicks} />
+    </div>
+  );
+};
 
 export default Calculator;
