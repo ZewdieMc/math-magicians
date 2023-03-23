acc16bc8143950e97ee56bd3232b1b3c1baa0473
ZewdieMc <zewdiehabtie26@gmail.com>
2023-03-23 20:18:00 +0300

    Render the Quote inside the App Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+/* eslint-disable linebreak-style */
 import Calculator from './components/Calculator';
 import './styles/calculator.css';
+import Quote from './components/Quote';
 
 function App() {
   return (
     <div className="app">
       <h1>Let&apos;s Do some math</h1>
       <Calculator />
+      <Quote />
     </div>
   );
 }
