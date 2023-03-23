df1e4556a5638eb87c40b8b5cd91cf081a674ecb
ZewdieMc <zewdiehabtie26@gmail.com>
2023-03-23 22:21:35 +0300

    Add Loading state:

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,32 +3,29 @@ import { useEffect, useState } from 'react';
 const Quote = () => {
   const baseURL = 'https://api.api-ninjas.com/v1/quotes';
   const [quotes, setQuote] = useState([]);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState('');
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    try {
-      fetch(`${baseURL}?limit=4&category=success`, {
-        limit: 4,
-        headers: {
-          'X-Api-Key': 'XVTYDqKegKHVDqR0pHEeSQ==MnYjSlly2QbA1h0u',
-        },
+    fetch(`${baseURL}?limit=4&category=success`, {
+      limit: 4,
+      headers: {
+        'X-Api-Key': 'XVTYDqKegKHVDqR0pHEeSQ==MnYjSlly2QbA1h0u',
+      },
+    })
+      .then((result) => {
+        setLoading(false);
+        setError('');
+        return result.json();
       })
-        .then((result) => {
-          setLoading(false);
-          return result.json();
-        })
-        .then((data) => setQuote(data));
-    } catch (e) {
-      setError('Failed fetching data from the API');
-      setLoading(false);
-    }
-  }, []); // TODO
+      .then((data) => setQuote(data))
+      .catch((err) => { setError(err); setLoading(false); });
+  }, []);
   return (
     <div className="quotes">
-      {error && <span>{error}</span>}
+      {error && <span className="error">Error while fetching data...</span>}
       {
         quotes.map((quote) => (
-          <div className="quoteContainer" key={quote}>
+          <div className="quoteContainer" key={quote.quote}>
             <p className="quote">{quote.quote}</p>
             <small className="author">{quote.author}</small>
           </div>
