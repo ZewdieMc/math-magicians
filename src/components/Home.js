a4540aad443f3b1d7ad91b41a66640395478e09e
ZewdieMc <zewdiehabtie26@gmail.com>
2023-03-28 17:03:01 +0300

    Add Home content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 const Home = () => (
-  <div>
+  <div className="home">
     <h2>Welcome to our home page</h2>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
       do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
     </p>
   </div>
 );
