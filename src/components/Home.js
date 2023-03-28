e150f033bb607dc2a7872602449f3168edede069
ZewdieMc <zewdiehabtie26@gmail.com>
2023-03-28 14:35:21 +0300

    Add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+const Home = () => (
+  <div>
+    <h2>Welcome to our home page</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </p>
+  </div>
+);
+
+export default Home;
